a4107804ad387fbe2c8bf599d2a7c704f94fc274
mlusk89 <mlusk89@gmail.com>
2024-11-12 11:13:20 -0700

    changes for +/- test activity (unfinished)

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -16,13 +16,25 @@ class LoginPage extends Page {
         return $('#login-button');
     }
 
-    
+    get error () {
+        return $('h3[data-test="error"]')
+    }
+
+    usernames= ["standard_user", "locked_out_user", "problem_user", 
+    "performance_glitch_user", "error_user", "visual_user"];
+
     async login (username, password) {
         await this.inputUsername.setValue(username);
         await this.inputPassword.setValue(password);
         await this.btnSubmit.click();
     }
 
+   async loopFunction(password){
+        for (let i = 0; i<this.usernames.length; i++){
+            this.login(usernames[i], password)
+            //add expect -- create function for +/- test
+        }
+   }
   
     open () {
         return super.open('login');
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -2,17 +2,18 @@ import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
 
 describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        
-        await LoginPage.open()
-        await LoginPage.login('standard_user', 'secret_sauce')
-        await expect(browser.url("https://www.saucedemo.com/inventory.html"))
-    })
-    it('should not login with invalid credentials', async () =>{
-        await LoginPage.open()
-        await LoginPage.login('standard_user', 'secrt_sauce')
-        const error1 = await $('h3[data-test="error"]')
-        await expect(error1).toHaveText('Epic sadface')
-    })
-})
+        it('should login with valid credentials for ${username}', async () => {
+       
+            await LoginPage.open()
+            await LoginPage.login(usernames, "secret_sauce")
+            await expect(browser.url("https://www.saucedemo.com/inventory.html"));
+            
+            //make changes to get rid of usernames.js -- check parameters call to login.page
+
+            if (await LoginPage.error.isExisting()){
+                expect(LoginPage.error).containing("incorrect")
+                expect(LoginPage.error).containing("locked");
+            }
+        });
+});
 
