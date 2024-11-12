935e0bcb75bd699d6849747002c208a0b14e120d
mlusk89 <mlusk89@gmail.com>
2024-11-11 20:17:35 -0700

    added negative

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -8,5 +8,11 @@ describe('My Login application', () => {
         await LoginPage.login('standard_user', 'secret_sauce')
         await expect(browser.url("https://www.saucedemo.com/inventory.html"))
     })
+    it('should not login with invalid credentials', async () =>{
+        await LoginPage.open()
+        await LoginPage.login('standard_user', 'secrt_sauce')
+        const error1 = await $('h3[data-test="error"]')
+        await expect(error1).toHaveText('Epic sadface')
+    })
 })
 
