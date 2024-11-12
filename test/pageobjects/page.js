b13141f2322a38ed71078635e303aa4c1c8a0454
mlusk89 <mlusk89@gmail.com>
2024-11-11 20:17:04 -0700

    removed comments

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -1,14 +1,6 @@
 import { browser } from '@wdio/globals';
 
-/**
-* main page object containing all methods, selectors and functionality
-* that is shared across all page objects
-*/
 export default class Page {
-    //**
-    //* Opens a sub page of the page
-    //* @param path path of the sub page
-    //*/
     open(){
         return browser.url('https://www.saucedemo.com');
     }
